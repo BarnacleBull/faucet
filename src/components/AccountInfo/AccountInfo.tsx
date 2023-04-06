b331480a05c35bc0304dc3f6fc733de52a2c9b18
BarnacleBull <129243792+BarnacleBull@users.noreply.github.com>
2023-04-05 23:49:35 -0400

    Clean up (#3)
    * Clean up
    
    * fix build

--- src/components/AccountInfo/AccountInfo.tsx
+++ src/components/AccountInfo/AccountInfo.tsx
@@ -3,47 +3,68 @@ import { IoCopySharp, IoSendSharp } from 'react-icons/io5';
 import { toast } from 'react-toastify';
 import { useQueryClient, useWallet } from '@sei-js/react';
 
-import { BalanceResponseType } from '../../types';
+import { BalanceResponseType } from '@sparrowswap/types';
+import {convertMicroDenomToDenom, formatTokenBalance} from '@sparrowswap/util'
+import { useRefetchQueries } from '@sparrowswap/hooks/useRefetchQueries';
 import styles from './styles.module.css'
-import {Card, CardContent, Divider, Stack} from '@mui/material'
+import {Card, CardContent, CircularProgress, Divider, Stack} from '@mui/material'
+import {useQuery} from "react-query";
 
 const AccountInfo = () => {
 	const { offlineSigner, accounts } = useWallet();
 	const { queryClient } = useQueryClient();
 
-	const [walletBalances, setWalletBalances] = useState<BalanceResponseType[]>([]);
-
 	const walletAccount = useMemo(() => accounts?.[0], [accounts]);
 
-	useEffect(() => {
-		const fetchBalances = async () => {
-			if (queryClient && walletAccount) {
-				const { balances } = await queryClient.cosmos.bank.v1beta1.allBalances({ address: walletAccount.address });
-				return balances as BalanceResponseType[];
-			}
-			return [];
-		};
+  const refetchBalances = useRefetchQueries(['balances'], 500)
 
-		fetchBalances().then(setWalletBalances);
-	}, [offlineSigner]);
+	useEffect(() => {
+		refetchBalances()
+	}, [offlineSigner, refetchBalances]);
+
+  const { data: walletBalances, isLoading: isLoadingBalances } = useQuery(
+    `balances`,
+  async () => {
+      const defaultBalance = {amount: "0", denom: process.env.NEXT_PUBLIC_DENOM} as BalanceResponseType
+    if (queryClient && walletAccount) {
+      const { balances } = await queryClient.cosmos.bank.v1beta1.allBalances({ address: walletAccount.address });
+      return balances.filter((balance) => balance.denom === process.env.NEXT_PUBLIC_DENOM) as BalanceResponseType[] || [];
+    }
+    return [];
+  },    {
+    enabled: Boolean(
+      walletAccount
+    ),
+
+      refetchOnMount: 'always',
+      refetchInterval: 15000,
+      refetchIntervalInBackground: true,
+
+      onError(error) {
+      console.error('Cannot fetch token balance bc:', error)
+    },
+  })
 
 	const renderBalances = () => {
 		if (!walletAccount) {
 			return <p>Wallet not connected</p>;
 		}
-		if (walletBalances.length === 0) {
+		if (walletBalances?.length === 0) {
 			return (
-				<div>
-					<p>No tokens available</p>
-				</div>
+        <CircularProgress color="inherit" size={25} />
 			);
 		}
 
 		return walletBalances?.map((balance) => {
 			return (
 				<Stack key={balance.denom} spacing={0.5}>
-					<div className={styles.tokenAmount}>{balance.amount}</div>
-					<div className={styles.tokenDenom}>{balance.denom}</div>
+					<div className={styles.tokenAmount}>{
+            formatTokenBalance(
+              convertMicroDenomToDenom(balance.amount, 6),
+              {
+                includeCommaSeparation: true
+              })
+          }&nbsp; USDC</div>
 				</Stack>
 			);
 		});
@@ -54,17 +75,19 @@ const AccountInfo = () => {
 		navigator.clipboard.writeText(walletAccount?.address || '').then();
 	};
 
+  if (!walletAccount?.address) {
+    return <></>
+  }
+
 	return (
 		<Card>
       <CardContent>
-
-
-      <h3>Account Info</h3>
+      <h3>Wallet Address</h3>
 				<Stack direction='row' alignItems='center'>
 					{walletAccount?.address || 'No account found!'}
           {walletAccount?.address && <IoCopySharp className={styles.copy} onClick={onClickCopy} />}
 				</Stack>
-        <h3>Balances</h3>
+        <h3>Balance</h3>
 				<Stack divider={<Divider flexItem />} spacing={2}>
           {renderBalances()}
         </Stack>
--- src/components/RequestButton/RequestButton.tsx
+++ src/components/RequestButton/RequestButton.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useMemo, useState } from 'react'
-import { toast } from 'react-toastify';
+import { toast } from 'react-toastify'
 import { useWallet } from '@sei-js/react'
 
 import {
@@ -9,16 +9,29 @@ import {
 import { useMutation, useQueryClient } from 'react-query'
 import { AccountData } from '@cosmjs/proto-signing'
 import axios from 'axios'
+import {useRefetchQueries} from "@sparrowswap/hooks/useRefetchQueries";
 
 const RequestButton = () => {
 	const { accounts } = useWallet();
-  const queryClient = useQueryClient()
 	const walletAccount = useMemo(() => accounts?.[0], [accounts]);
+
+  const refetchBalances = useRefetchQueries(['balances'], 1500)
 
   const { mutate: requestFaucet, isLoading } = useMutation(
     async (account: AccountData) => {
       const { data: response } = await axios.get('/api/faucet', {params: {dest: account.address}})
       console.log(response.data)
+    },
+    {
+      onSuccess: () => {
+        console.log('success')
+        toast.success('Faucet request successful!')
+        refetchBalances()
+      },
+      onError: (error) => {
+        console.log(error)
+        toast.error((error as any)?.message ?? error?.toString())
+      }
     }
   )
 
